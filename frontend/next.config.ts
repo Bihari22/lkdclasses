444b08a31d5843791c0a1dd0a3e762a425a5a3ab
vrscodeverse <vrscodeverse@gmail.com>
2025-10-07 06:49:07 +0530

    Quick deploy fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+const nextConfig = {
+  output: "export", // makes the build fully static
+  eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
+
